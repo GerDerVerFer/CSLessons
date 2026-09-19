4481d479bc1062970dbb6514d73eb68e7b85283d
name <m.chistyakov@univ.priv>
2026-09-19 09:50:19 +0300

    init

---
+++ Patterner/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Singleton.Run();
+        // Prototype.Run();
+        // Builder.Run(); 
+        // Factory.Run();
+        AbstractFactory.Run();
+    }
+}
